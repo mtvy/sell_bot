61cdb0a2d4d6bf26a43d9948b129394f5dfb6510
Mtvy <m.d.prudnik@gmail.com>
2023-05-22 00:50:49 +0300

    refact

--- cases/db/db.py
+++ cases/db/db.py
@@ -1,6 +1,6 @@
 from .grpc_py_database_accessing.client import Database
 
 DBHOST = 'postgres://postgres:postgres@postgres:5433/postgres'
-HOST = '0.0.0.0:8080'
+HOST = 'accessor:8080'
 
 db = Database(HOST, DBHOST)
